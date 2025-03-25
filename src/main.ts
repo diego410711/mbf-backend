54a7eeffe1b940d34d41a626b13f06ed49db0c7a
diego41071 <alexander.diego.2007@hotmail.com>
2025-03-25 00:59:30 -0500

    cambios en servidor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Habilita CORS
+  app.use(bodyParser.json({ limit: '50mb' })); // Ajusta según necesidad
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(3000);
 }
 bootstrap();
